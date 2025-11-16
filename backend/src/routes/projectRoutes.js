7290586a8b16461b9069251b00886e52d27f4692
Damien LUBET <damien.lubet@etu.u-bordeaux.fr>
2025-11-16 23:15:59 +0100

    feat: add endpoint members management

--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { createProject, deleteProject, editProject, getProjectbyId } from '../controllers/projectController.js';
+import { addProjectMember, changeMemberRole, removeProjectMember } from '../controllers/projectMemberController.js';
+import exists from '../middleware/exists.js';
 import requireAuth from '../middleware/requireAuth.js';
+import { requireScrumMaster } from '../middleware/requireRole.js';
+import Project from '../models/project.js';
 
 
 const projectRouter = express.Router();
@@ -12,5 +16,10 @@ projectRouter.post('/projects', createProject);
 projectRouter.get('/projects/:id', getProjectbyId);
 projectRouter.delete('/projects/:id', deleteProject);
 projectRouter.put('/projects/:id', editProject);
+
+// manage project members routes, need Scrum Master role
+projectRouter.post('/projects/:id/members', exists(Project, 'id'), requireScrumMaster, addProjectMember);
+projectRouter.delete('/projects/:id/members', exists(Project, 'id'), requireScrumMaster, removeProjectMember);
+projectRouter.put('/projects/:id/members', exists(Project, 'id'), requireScrumMaster, changeMemberRole);
 
 export default projectRouter;
