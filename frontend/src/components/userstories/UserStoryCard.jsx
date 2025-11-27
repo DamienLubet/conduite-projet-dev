d521ba4ab5b2cd3788d0a2b6d6c8a801f025fe9c
Damien LUBET <damien.lubet@etu.u-bordeaux.fr>
2025-11-27 17:03:35 +0100

    feat(T8.1): add US list page

---
+++ frontend/src/components/userstories/UserStoryCard.jsx
@@ -0,0 +1,27 @@
+export default function UserStoryCard({ 
+    story
+}) {
+
+    return (
+        <div>
+            <div className="userstory-card">
+                <div className="userstory-card-header">
+    
+                    <div className="userstory-card-header-left">
+                        <span className="userstory-number">US{story.number}</span>
+                        <span className="userstory-title">{story.title}</span>
+                    </div>
+                    
+                    <span className={`userstory-priority userstory-priority-${story.priority.toLowerCase()}`}>{story.priority}</span>
+                    <span className="userstory-storypoints">{story.storyPoints} SP</span>
+                </div>
+                
+                {story.description && (<p className="userstory-description">{story.description}</p>)}
+                
+                <div className="userstory-card-footer">
+                    <button className='create-task-button'>New Task</button>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/components/userstories/UserStoryList.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { userStoryApi } from '../../api/userstoryApi';
+import '../../styles/userStoryStyle.css';
+import UserStoryCard from './UserStoryCard.jsx';
+
+export default function UserStoryList() {
+    const { getUserStoriesByProject, createUserStory, updateUserStory, deleteUserStory } = userStoryApi();
+    const { projectId } = useParams();
+    
+    const [userStories, setUserStories] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    const fetchUserStories = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const res = await getUserStoriesByProject(projectId);
+            setUserStories(res.data);
+        } catch (err) {
+            setError('Failed to load user stories.');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchUserStories();
+    }, [projectId]);
+
+    return (
+        <div className="userstory-page">
+            {loading && <p>Loading...</p>}
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            
+            <div className="userstory-list-container">
+                <div className="userstory-header">
+                    <h3>Backlog  {'('} {userStories.length} User Stories {')'}</h3>
+                    <button
+                        className="us-add-button"
+                        onClick={() => setShowCreateModal(true)}
+                    >
+                        Create User Story
+                    </button>
+                </div>
+                
+                <div className="userstory-card-column">
+                    {Array.isArray(userStories) && userStories.map((story) => (
+                        <UserStoryCard 
+                            key={story._id}
+                            story={story}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
