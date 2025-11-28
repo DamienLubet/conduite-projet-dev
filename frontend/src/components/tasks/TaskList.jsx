3f763f253edda4a087e46b3f3eee399d29c0f12e
Damien LUBET <damien.lubet@etu.u-bordeaux.fr>
2025-11-28 22:43:37 +0100

    feat: modify US card to expands and view tasks lists

---
+++ frontend/src/components/tasks/TaskList.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import '../../styles/taskStyle.css';
+import TaskDeleteConfirm from './TaskDeleteConfirm.jsx';
+import TaskEdit from './TaskEdit.jsx';
+
+export default function TaskList({ tasks, userStoryNumber, onUpdated }) {
+    // Modal states : Edit, Delete
+    const [modalState, setModalState] = useState({ type: null, data: null });
+    const closeModal = () => setModalState({ type: null, data: null });
+    
+    return (
+        <div className="task-list">
+            <div className="task-card-column">
+                {Array.isArray(tasks) && tasks.map((task) => (
+                    <div key={task._id} className="task-card" onClick={() => setModalState({ type: 'EDIT', data: task })}>
+                        <div className="task-card-header">
+                            <div className="task-card-header-left">
+                                <span className="task-number">T{userStoryNumber}.{task.number}</span>
+                                <span className="task-title">{task.title}</span>
+                            </div>
+                            <span className="task-status">{task.status}</span>
+                            {task.assignee && <span className="task-assignee">{task.assignee.username}</span>}
+                        </div>
+                        {task.description && <p className="task-description">{task.description}</p>}
+                    </div>
+                ))}
+            </div>
+        
+            {modalState.type === 'EDIT' && (
+                <TaskEdit 
+                    task={modalState.data}
+                    onUpdated={async () => {
+                        closeModal();
+                        await onUpdated();
+                    }}
+                    onCancel={() => closeModal()}
+                    onRequestDelete={() => {
+                        setModalState({ type: 'DELETE', data: modalState.data });
+                    }}
+                />
+            )}
+            {modalState.type === 'DELETE' && (
+                <TaskDeleteConfirm
+                    task={modalState.data}
+                    onDeleted={async () => {
+                        closeModal();
+                        await onUpdated();
+                    }}
+                    onCancel={() => closeModal()}
+                />
+            )}
+        </div>
+                
+    );
+}
