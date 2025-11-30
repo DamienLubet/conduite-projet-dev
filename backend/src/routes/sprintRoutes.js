31ad2ea4c70169501f0757cd42ab2628fb1b82e1
unknown <bastien.rocher@etu.u-bordeaux.fr>
2025-11-30 19:46:22 +0100

    Updated sprintRoutes to give access to DeletSprint

--- backend/src/routes/sprintRoutes.js
+++ backend/src/routes/sprintRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { assignUserStoriesToSprint, createSprint, getSprintsByProject, updateSprint, startSprint, completeSprint } from '../controllers/sprintController.js';
+import { assignUserStoriesToSprint, createSprint, getSprintsByProject, updateSprint, deleteSprint, startSprint, completeSprint } from '../controllers/sprintController.js';
 import exists from '../middleware/exists.js';
 import requireAuth from '../middleware/requireAuth.js';
 import { requireScrumMaster, requireToBeMember } from '../middleware/requireRole.js';
@@ -15,6 +15,7 @@ sprintRouter.use(requireAuth);
 sprintRouter.post('/projects/:projectId/sprints', exists(Project, 'projectId'), requireScrumMaster, createSprint);
 sprintRouter.get('/projects/:projectId/sprints', exists(Project, 'projectId'), requireToBeMember, getSprintsByProject);
 sprintRouter.put('/sprints/:sprintId', exists(Sprint, 'sprintId'), resolveProjectId, requireScrumMaster, updateSprint);
+sprintRouter.delete('/sprints/:sprintId', exists(Sprint, 'sprintId'), resolveProjectId, requireScrumMaster, deleteSprint);
 sprintRouter.post('/sprints/:sprintId/userstories', exists(Sprint, 'sprintId'), resolveProjectId, requireScrumMaster, assignUserStoriesToSprint);
 sprintRouter.post('/sprints/:sprintId/start', exists(Sprint, 'sprintId'), resolveProjectId, requireScrumMaster, startSprint);
 sprintRouter.post('/sprints/:sprintId/complete', exists(Sprint, 'sprintId'), resolveProjectId, requireScrumMaster, completeSprint);
