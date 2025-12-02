5df3b18417a86e3ebf0f78d286fbfc4ac737b417
Damien LUBET <damien.lubet@etu.u-bordeaux.fr>
2025-12-02 18:45:01 +0100

    feat(backend): better permission to project access, task and us

--- backend/src/routes/userstoryRoutes.js
+++ backend/src/routes/userstoryRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { createUserStory, deleteUserStory, getUserStoriesByProject, getUserStoryById, updateUserStory } from '../controllers/userstoryController.js';
 import exists from '../middleware/exists.js';
 import requireAuth from '../middleware/requireAuth.js';
-import { requireToBeMember } from '../middleware/requireRole.js';
+import { requireScrumMaster, requireToBeMember } from '../middleware/requireRole.js';
 import resolveProjectId from '../middleware/resolveProjectId.js';
 import Project from '../models/project.js';
 import UserStory from '../models/userstory.js';
@@ -13,10 +13,10 @@ const userstoryRouter = express.Router();
 // Protect route (need to be authenticated)
 userstoryRouter.use(requireAuth);
 
-userstoryRouter.post('/projects/:projectId/userstories', createUserStory);
+userstoryRouter.post('/projects/:projectId/userstories', exists(Project, 'projectId'), requireScrumMaster, createUserStory);
 userstoryRouter.get('/projects/:projectId/userstories', exists(Project, 'projectId'), requireToBeMember, getUserStoriesByProject);
 userstoryRouter.get('/userstories/:id', exists(UserStory, 'id'), resolveProjectId, requireToBeMember, getUserStoryById);
-userstoryRouter.put('/userstories/:id', exists(UserStory, 'id'), resolveProjectId, requireToBeMember, updateUserStory);
-userstoryRouter.delete('/userstories/:id', exists(UserStory, 'id'), resolveProjectId, requireToBeMember, deleteUserStory);
+userstoryRouter.put('/userstories/:id', exists(UserStory, 'id'), resolveProjectId, requireScrumMaster, updateUserStory);
+userstoryRouter.delete('/userstories/:id', exists(UserStory, 'id'), resolveProjectId, requireScrumMaster, deleteUserStory);
 
 export default userstoryRouter;
